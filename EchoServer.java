bbc64b36b6503069193f03e809f4a999bc40c2eb
mattloidolt <mattl@opdemand.com>
2012-06-13 10:14:28 -0600

    changed to just the server, added functions

--- EchoServer.java
+++ EchoServer.java
@@ -4,6 +4,7 @@
 
 import java.io.*;
 import ocsf.server.*;
+import java.util.ArrayList;
 
 /**
  * This class overrides some of the methods in the abstract 
@@ -23,6 +24,8 @@ public class EchoServer extends AbstractServer
    * The default port to listen on.
    */
   final public static int DEFAULT_PORT = 5555;
+  ArrayList<String> users;
+  String callingUser = "";
   
   //Constructors ****************************************************
   
@@ -34,6 +37,7 @@ public class EchoServer extends AbstractServer
   public EchoServer(int port) 
   {
     super(port);
+    users = new ArrayList<String>();
   }
 
   
@@ -49,61 +53,111 @@ public class EchoServer extends AbstractServer
     (Object msg, ConnectionToClient client)
   {
     System.out.println("Message received: " + msg + " from " + client);
-    this.sendToAllClients(msg);
+		if(((String) msg).startsWith("#login ")) {
+			if(client.getInfo("loginID") == ( (String) msg).substring(7) ){
+				try {
+					client.sendToClient("You cannot login twice.") ;
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+			else {
+				client.setInfo("loginID", ( (String) msg).substring(7) ) ;
+				users.add( ((String) msg).substring(7) );
+			}
+		}
+		///////////////////////////////////////////////////////////////////
+		else if(((String) msg).startsWith("#whoblocksme")){
+			this.sendToAllClients("#doyoublock " + client.getInfo("loginID")) ;
+			// save the calling user
+			callingUser = (String) client.getInfo("loginID") ;
+		}
+		else if(((String) msg).startsWith("#ifidoblock")){
+			/* index to determine location of each block, corresponds to index in
+		 	arraylist of users*/
+			
+			if(((String) msg).charAt(12) == 'y')
+				this.sendToAllClients("#whoblocksme " + callingUser + " Messages to "
+						+ (String) client.getInfo("loginID") + " are being blocked.") ;
+		}
+		///////////////////////////////////////////////////////////////////////////
+		else {
+			this.sendToAllClients(client.getInfo("loginID") + " >>  " + msg.toString());
+		}
   }
     
   /**
-   * This method overrides the one in the superclass.  Called
-   * when the server starts listening for connections.
-   */
-  protected void serverStarted()
-  {
-    System.out.println
-      ("Server listening for connections on port " + getPort());
-  }
-  
-  /**
-   * This method overrides the one in the superclass.  Called
-   * when the server stops listening for connections.
-   */
-  protected void serverStopped()
-  {
-    System.out.println
-      ("Server has stopped listening for connections.");
-  }
-  
-  //Class methods ***************************************************
-  
-  /**
-   * This method is responsible for the creation of 
-   * the server instance (there is no UI in this phase).
-   *
-   * @param args[0] The port number to listen on.  Defaults to 5555 
-   *          if no argument is entered.
-   */
-  public static void main(String[] args) 
-  {
-    int port = 0; //Port to listen on
-
-    try
-    {
-      port = Integer.parseInt(args[0]); //Get port from command line
-    }
-    catch(Throwable t)
-    {
-      port = DEFAULT_PORT; //Set port to 5555
-    }
+	 * 
+	 */
+	public void clientConnected(ConnectionToClient client){
+		System.out.println("The client "+client+" has connected.");
+	}
+	  
+	/**
+	 * 
+	 */
+	public void clientDisconnected(ConnectionToClient client){
+		System.out.println("The client "+client +" has disconnected.");
+		//users.remove(client.getInfo("loginID")) ;
+	}
 	
-    EchoServer sv = new EchoServer(port);
-    
-    try 
-    {
-      sv.listen(); //Start listening for connections
-    } 
-    catch (Exception ex) 
-    {
-      System.out.println("ERROR - Could not listen for clients!");
-    }
-  }
+	/**
+	 * This method overrides the one in the superclass. Called when the server
+	 * starts listening for connections.
+	 */
+	protected void serverStarted() {
+		System.out.println("Server listening for connections on port "
+				+ getPort());
+	}
+
+	/**
+	 * Checks if a user is on the list of current valid users.  Persists even
+	 * if a user logs off
+	 * 
+	 * @param user
+	 * @return
+	 */
+	public boolean doesUserExist(String user){
+		return (users.contains(user));
+	}
+	
+	/**
+	 * This method overrides the one in the superclass. Called when the server
+	 * stops listening for connections.
+	 */
+	protected void serverStopped() {
+		System.out.println("Server has stopped listening for connections.");
+	}
+
+	// Class methods ***************************************************
+
+	/**
+	 * This method is responsible for the creation of the server instance (there
+	 * is no UI in this phase).
+	 * 
+	 * @param args
+	 *            [0] The port number to listen on. Defaults to 5555 if no
+	 *            argument is entered.
+	 */
+	
+	 public static void main(String[] args) { 
+		 int port = 0; //Port to listen on
+
+		 try { 
+			 port = Integer.parseInt(args[0]); //Get port from command line 
+		 }
+		 catch(Throwable t) { 
+			 port = DEFAULT_PORT; //Set port to 5555
+		 }
+	 
+		 EchoServer sv = new EchoServer(port);
+		 ServerConsole server = new ServerConsole(sv);
+		 try { 
+			 sv.listen(); //Start listening for connections 
+		 } catch (Exception ex) { 
+			 System.out.println("ERROR - Could not listen for clients!");
+		 }
+		 server.accept();
+	 }
 }
 //End of EchoServer class
---
+++ ServerConsole.java
@@ -0,0 +1,149 @@
+//cs314 P1 Jon McQuillan, Matt Loidolt, Zach Cable
+
+import java.io.*;
+
+import common.*;
+import ocsf.server.*;
+import ocsf.client.*;
+
+public class ServerConsole implements ChatIF {
+
+	EchoServer server; //instance of Echoserver
+	
+	/**
+	 * Constructor to be able to use the current server
+	 * @param server
+	 */
+	ServerConsole(EchoServer server){
+		this.server = server;
+	}
+	
+
+	/**
+	 * accepts input to the server console.  Sends input formatted as commands
+	 * to executeServerCommand()
+	 */
+	public void accept() {
+		try {
+			BufferedReader fromConsole = new BufferedReader(new InputStreamReader(System.in));
+			String message;
+
+			while (true) {
+				message = fromConsole.readLine();
+				if (!message.startsWith("#", 0)) {
+					System.out.println("Message received: " + message + " from server");
+					display(message);
+				} else {
+					executeServerCommand(message);
+				}
+			}
+		} catch (Exception ex) {
+			System.out.println("Unexpected error while reading from console!");
+		}
+	}
+	
+	/**
+	 * executes various valid commands given to the server console.
+	 * 
+	 * @param message : command to be executed
+	 */
+	public void executeServerCommand(String message){
+		try{
+			if (message.equals("#quit")) {//quits the server and closes the program
+				quit();
+			} else if (message.equals("#stop")) {//stops the server from listening for new connections
+				stop();
+			} else if (message.equals("#close")) {//closes the server to all clients
+				close();
+			} else if (message.startsWith("#setport")) {//sets a new server port
+					int port = Integer.parseInt(message.substring(8, message.length() - 1).trim());
+					setPort(port);
+			} else if (message.equals("#start")) {//starts a server listening for connections
+				start();
+			} else if (message.equals("#getport")) {//gets the current port
+				getPort();
+			} else {//default message if illegal command issued
+				System.out.println("Valid commands prefaced with '#' are: quit,"
+						+ " stop, close, setport <port>, start, and getport.");
+			}
+		} catch(Exception ex) {
+			System.out.println("Unexpected error while reading from console!");
+		}
+	}
+	
+	/**
+	 * Quits the server and sends a message to all clients that the server is quitting
+	 * @throws IOException
+	 */
+	private void quit() throws IOException{
+		display("Server is exiting.");
+		server.close();
+		System.out.println("Server is exiting.");
+		System.exit(0);
+	}
+	
+	/**
+	 * Stops the server from listening for new connections
+	 */
+	private void stop(){
+		display("Server has stopped listening for new connections.");
+		server.stopListening();
+	}
+	
+	/**
+	 * Closes the server for all connections
+	 * @throws IOException
+	 */
+	private void close() throws IOException{
+		display("Server is closed.");
+		server.close();
+	}
+	
+	/**
+	 * Sets a new port for the server to listen on if the server is closed
+	 * @param port New port for the server to lisen on
+	 */
+	private void setPort(int port){
+		if (!server.isListening()) {
+			display("Server port now set to: " + port);
+			server.setPort(port);
+		} else {
+			System.out.println("Server needs to be closed to change port.");
+		}
+	}
+	
+	/**
+	 * Starts a server listening for new connections
+	 * @throws IOException
+	 */
+	private void start() throws IOException{
+		if (!server.isListening()) {
+			display("Server has started listening for new connections.");
+			server.listen();
+		} else {
+			System.out.println("Server is already started.");
+		}
+	}
+	
+	/**
+	 * Gets the port that the server is listening on and prints it to the console
+	 */
+	private void getPort(){
+		int port = server.getPort();
+		display("Server port is: " + port);
+	}
+
+	/**
+	 * This method overrides the method in the ChatIF interface. It displays a
+	 * message onto the screen.
+	 * 
+	 * @param message The string to be displayed.
+	 */
+	public void display(String message) {
+		//todo
+		//change so server sends all messages as user SERVER MSG
+		server.getClientConnections();
+		String msg = "SERVER MSG> " + message;
+		server.sendToAllClients(msg);
+	}
+}
